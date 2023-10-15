afbd81eec8b6539ecbc1986cca78cc0e72d40bd9
bayonasam <bayonasam@gmail.com>
2023-10-15 21:57:05 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -8,7 +8,7 @@ DNS_WORDLIST="$BASE_PATH/data/wordlists/dns"
 apt install jq -y
 
 # download custom dns/domains/subdomains wordlists 
-wget -O "$DNS_WORDLIST/2m-subdomains.txt" https://wordlists-cdn.assetnote.io/data/manual/2m-subdomains.txt
+wget -o "$DNS_WORDLIST/2m-subdomains.txt" https://wordlists-cdn.assetnote.io/data/manual/2m-subdomains.txt
 wget -o "$DNS_WORDLIST/best-dns-wordlist.txt" https://wordlists-cdn.assetnote.io/data/manual/best-dns-wordlist.txt
 wget -o "$DNS_WORDLIST/httparchive_html_htm_2023_08_28.txt" https://wordlists-cdn.assetnote.io/data/automated/httparchive_html_htm_2023_08_28.txt
 wget -o "$DNS_WORDLIST/subdomains-spanish.txt" https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/subdomains-spanish.txt

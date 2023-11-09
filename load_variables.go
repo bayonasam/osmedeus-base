f2424d072c5c11bd6da5238d7668b955c28fc692
bayonasam <bayonasam@gmail.com>
2023-11-09 17:59:10 +0100

    Update load_variables.go

--- load_variables.go
+++ load_variables.go
@@ -9,17 +9,15 @@ import (
 
 func LoadEnvVariables() {
 
-        // Cargar configuración desde el archivo .yaml
-        viper.SetConfigType("yaml") // o viper.SetConfigName("config")
-        viper.AddConfigPath("/root/osmedeus-base/token/")    // Aquí asumimos que el archivo .yaml está en el mismo directorio
-        viper.SetConfigFile("/root/osmedeus-base/token/osm-var.yaml") // Aquí asumimos que tu archivo se llama config.yaml
+        viper.SetConfigType("yaml")
+        viper.SetConfigFile("/root/osmedeus-base/token/osm-var.yaml")
 
         err := viper.ReadInConfig()
         if err != nil {
                 panic(fmt.Errorf("fatal error config file: %w", err))
         }
 
-        // Leer y configurar las variables de entorno desde la sección de tokens
+        // Load and set variables
         tokens := viper.GetStringMapString("tokens")
         for key, val := range tokens {
                 os.Setenv(key, val)

6196a22b63ce3fa19c6b00cc12b75eda882dc187
bayonasam <bayonasam@gmail.com>
2023-11-14 20:52:51 +0100

    Update AmassApiLoader.sh

--- binaries/AmassApiLoader.sh
+++ binaries/AmassApiLoader.sh
@@ -4,13 +4,13 @@
 # This script is designed to be executed within an Osmedeus YAML configuration file prior to running Amass.
 # It automates the process of updating the Amass configuration file with environment variable values. 
 # Specifically, it copies the original datasources.yaml to datasources_token.yaml, then
-# scans through all environment variables. For each variable, if its name is found in datasources_token.yaml, the script
+# scans through all environment variables. For each variable, if its name is found in config.ini, the script
 # replaces occurrences of that variable name in the file with its corresponding value from the environment. This ensures
 # that Amass is executed with the current API tokens and settings defined in the environment.
 
 # Define paths for the original and new datasources files.
-ORIGINAL_CONFIG_FILE="/root/osmedeus-base/data/amass-config/datasources.yaml"
-NEW_CONFIG_FILE="/root/osmedeus-base/data/amass-config/datasources_custom.yaml"
+ORIGINAL_CONFIG_FILE="/root/osmedeus-base/data/amass-config/config.ini"
+NEW_CONFIG_FILE="/root/osmedeus-base/data/amass-config/config-api.ini"
 
 # Check if the original configuration file exists.
 if [ ! -f "$ORIGINAL_CONFIG_FILE" ]; then
@@ -35,6 +35,6 @@ while read -r variable_env; do
         var_value=$(printenv "$escaped_value")
 
         # Replace the variable name in the file with its value.
-        sed -i "s/$var_name/$var_value/g" "$NEW_CONFIG_FILE"
+        sed -i "s|$var_name|$var_value|g" "$NEW_CONFIG_FILE"
     fi      
 done < <(env)
